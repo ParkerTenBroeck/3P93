7396d11694d6044d468217bebbe9afbfaf27899b
ParkerTenBroeck <51721964+ParkerTenBroeck@users.noreply.github.com>
2025-10-09 15:43:36 -0400

    meow

--- p2/code/make_mp4.sh
+++ p2/code/make_mp4.sh
@@ -1,3 +1,3 @@
 cd animation
 
-ffmpeg -framerate 30 -pattern_type glob -i 'frame_*.png' -vf "scale='if(gt(iw/ih,1),min(480,iw),-1)':'if(gt(iw/ih,1),-1,min(480,ih))'" -vcodec libx264 -crf 23 -preset medium -acodec aac -b:a 192k out.mp4 
+ffmpeg -framerate 30 -pattern_type glob -i 'frame_*.png' -vcodec libx264 -crf 23 -preset medium -acodec aac -b:a 192k out.mp4
--- p2/code/make_webp.sh
+++ p2/code/make_webp.sh
@@ -1,3 +1,3 @@
 cd animation
 
-ffmpeg -framerate 30 -pattern_type glob -i 'frame_*.png' -vf "scale='if(gt(iw/ih,1),min(480,iw),-1)':'if(gt(iw/ih,1),-1,min(480,ih))'"  -c:v libwebp_anim -loop 0 -quality 95 out.webp
+ffmpeg -framerate 30 -pattern_type glob -i 'frame_*.png' -c:v libwebp_anim -loop 0 -quality 95 out.webp
