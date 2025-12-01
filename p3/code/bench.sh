4a8f2e070524347e005ae20a70439a9f01092567
Parker TenBroeck <51721964+ParkerTenBroeck@users.noreply.github.com>
2025-12-01 14:04:00 -0500

    bench more

--- p3/code/bench.sh
+++ p3/code/bench.sh
@@ -11,18 +11,24 @@ commands=(
 )
 
 params=(
-    "halo720:--scene=halo --width=720 --height=480 --write_frames=false --frames=300 --framerate=30"
-    "halo1080:--scene=halo --width=1920 --height=1080 --write_frames=false --frames=300 --framerate=30"
-    "bricks720:--scene=bricks --height=720 --width=480 --write_frames=false --frames=300 --framerate=30"
-    "bricks1080:--scene=bricks --height=1920 --width=1080 --write_frames=false --frames=300 --framerate=30"
-    "wavy720:--scene=test --width=720 --height=480 --write_frames=false --frames=300 --framerate=30"
-    "wavy1080:--scene=test --width=1920 --height=1080 --write_frames=false --frames=300 --framerate=30"
-    "halo720-f:--scene=halo --width=720 --height=480 --write_frames=true --frames=300 --framerate=30"
-    "halo1080-f:--scene=halo --width=1920 --height=1080 --write_frames=true --frames=300 --framerate=30"
-    "bricks720-f:--scene=bricks --height=720 --width=480 --write_frames=true --frames=300 --framerate=30"
-    "bricks1080-f:--scene=bricks --height=1920 --width=1080 --write_frames=true --frames=300 --framerate=30"
-    "wavy720-f:--scene=test --width=720 --height=480 --write_frames=true --frames=300 --framerate=30"
-    "wavy1080-f:--scene=test --width=1920 --height=1080 --write_frames=true --frames=300 --framerate=30"
+    "halo360:--scene=halo --width=360 --height=240 --write_frames=false --frames=300 --framerate=30"
+    #"halo720:--scene=halo --width=720 --height=480 --write_frames=false --frames=300 --framerate=30"
+    #"halo1080:--scene=halo --width=1920 --height=1080 --write_frames=false --frames=300 --framerate=30"
+    "bricks360:--scene=bricks --height=360 --width=240 --write_frames=false --frames=300 --framerate=30"
+    #"bricks720:--scene=bricks --height=720 --width=480 --write_frames=false --frames=300 --framerate=30"
+    #"bricks1080:--scene=bricks --height=1920 --width=1080 --write_frames=false --frames=300 --framerate=30"
+    "wavy360:--scene=test --width=360 --height=240 --write_frames=false --frames=300 --framerate=30"
+    #"wavy720:--scene=test --width=720 --height=480 --write_frames=false --frames=300 --framerate=30"
+    #"wavy1080:--scene=test --width=1920 --height=1080 --write_frames=false --frames=300 --framerate=30"
+    "halo360-f:--scene=halo --width=360 --height=240 --write_frames=true --frames=300 --framerate=30"
+    #"halo720-f:--scene=halo --width=720 --height=480 --write_frames=true --frames=300 --framerate=30"
+    #"halo1080-f:--scene=halo --width=1920 --height=1080 --write_frames=true --frames=300 --framerate=30"
+    "bricks360-f:--scene=bricks --height=360 --width=240 --write_frames=true --frames=300 --framerate=30"
+    #"bricks720-f:--scene=bricks --height=720 --width=480 --write_frames=true --frames=300 --framerate=30"
+    #"bricks1080-f:--scene=bricks --height=1920 --width=1080 --write_frames=true --frames=300 --framerate=30"
+    "wavy360-f:--scene=test --width=360 --height=240 --write_frames=true --frames=300 --framerate=30"
+    #"wavy720-f:--scene=test --width=720 --height=480 --write_frames=true --frames=300 --framerate=30"
+    #"wavy1080-f:--scene=test --width=1920 --height=1080 --write_frames=true --frames=300 --framerate=30"
 )
 
 for cmd_entry in "${commands[@]}"; do
