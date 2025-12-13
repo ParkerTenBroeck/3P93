dec9a1f00836d5d721b0e2755d7f463b31aa746d
Parker TenBroeck <51721964+ParkerTenBroeck@users.noreply.github.com>
2025-12-12 22:38:54 -0500

    final submission

--- p3/code/run_brick_1080.sh
+++
@@ -1,3 +0,0 @@
-cd build
-
-./rasterizer --scene=test --width=1920 --height=1080 --write_frames=true
--- p3/code/run_brick_720.sh
+++
@@ -1,3 +0,0 @@
-cd build
-
-./rasterizer --scene=test --width=720 --height=480 --write_frames=true 
